ad6f2018f8d8bb60194bf59bcfd8cfba52325de5
Chandler <chandle@email.unc.edu>
2022-02-08 01:57:16 -0500

    finished ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -3,7 +3,9 @@
 
 __author__ = 730403454
 
+
 i: int = 0
+
 
 def contains_char(a: str, b: str) -> bool:
     """Is b within a?"""
@@ -25,6 +27,8 @@ def contains_char(a: str, b: str) -> bool:
             else:
                 return False
         i += 1
+
+
 def emojified(guess: str, secret: str) -> str:
     """Yellow, white, or green."""
     assert len(guess) == len(secret)
@@ -42,8 +46,35 @@ def emojified(guess: str, secret: str) -> str:
             python += YELLOW_BOX
         p += 1
     return python
+
+
 def input_guess(x: int) -> str:
-    guessy: str = input(f"Enter a {x} character word:")
+    guessy: str = input(f"Enter a {x} character word: ")
     while len(guessy) != x:
         guessy: str = input(f"That wasn't {x} chars! Try again: ")
     return guessy
+
+
+def main() -> None:
+    """The entrypoint and main game loop."""
+    s: str = "codes"
+    v: bool = False
+    q: int = 6
+    turn_count: int = 1
+    while turn_count <= q and v is False:
+        print(f"=== Turn {turn_count}/{q} ===")
+        r: str = input_guess(5)
+        colors: str = emojified(r, s)
+        print(colors)
+        if r == s:
+            print(f"You won in {turn_count}/{q} turns!")
+            v = True
+        else:
+            turn_count += 1
+    if turn_count > q and v is False:
+        print(f"X/{q} - Sorry, try again tomorrow!")
+    return 
+
+
+if __name__ == "__main__":
+    main()
